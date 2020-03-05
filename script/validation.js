4a6a8c2c4c11330f115e04211d88a0dae25287dd
Viktor Pylypenko <vitya.slot@gmail.com>
2020-03-05 16:26:08 +0200

    username log

--- script/validation.js
+++ script/validation.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 
 function checkAnswer(answerPromise) {
     if (!(/^(?!(?:null|undefined|false|[1-6]|^aa$))[\w\d_]{1,30}$/g).test(answerPromise)) {
-      console.log("Username is incorrect. Please re-enter")
+      console.log("The parameter 'username' must not contain: spaces, dots, cyrillic and must not be null, false, undefined, 123456. Also your username cannot be longer than 30 characters. ")
       return false
     } 
     return true
